4a26fb521f0c1d02bd8fdf685f60abb3638ef9df
Shivam030902 <153266080+Shivam030902@users.noreply.github.com>
2024-01-09 16:05:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function hello(){
-	console.log("alert"+"("+'"'+arguments.callee.name+'"'+")");
+	console.log("alert"+"("+'"'+arguments.callee.name+'"'+")"+";");
 }
 
 hello();
